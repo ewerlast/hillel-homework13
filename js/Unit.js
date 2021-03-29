727863ba67b20ca32072a99083eb703c4f5738d7
ewerlast1984 <ewerlast1984@gmail.com>
2021-03-29 15:57:09 +0300

    homework13/test

--- js/Unit.js
+++
@@ -1,7 +0,0 @@
-function Unit(type, health, maxHealth, maxDistance) {
-    this.type = type;
-    this.health = health;
-    this.maxHealth = maxHealth;
-    this.maxDistance = maxDistance;
- }
- 
--- js/isReadyToMove.js
+++
@@ -1,9 +0,0 @@
-function isReadyToMove (maxDistance){
-    let result = "да"
-    this.maxDistance = maxDistance;
-    if(this.maxDistance > 100){
-        result = "нет";
-    }
-    return result;
-   
-}
